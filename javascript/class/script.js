b1843c7f92e58258e6ec36a432293c074808ec68
mahmadasghar <muhammadahmadb1234@gmail.com>
2024-01-01 12:26:24 +0500

    upload dev-dreamer

---
+++ javascript/class/script.js
@@ -0,0 +1,8 @@
+// what is javascript?
+//          Javascript is an object based scripting language that is used to
+//          make websites dynamically  an intractive.
+    //   Funcation:
+    //          Changes in websites without reload page or website.
+    //              For Example:
+    //                      Current clock time cannot do the html css alone.
+    // 
